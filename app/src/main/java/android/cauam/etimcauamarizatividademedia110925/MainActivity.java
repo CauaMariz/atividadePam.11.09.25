839dcf27ca0bc529ca0cd77e0b1564f085d62dba
android <Android@uirapuru.br>
2025-09-11 15:15:45 -0300

    x

---
+++ app/src/main/java/android/cauam/etimcauamarizatividademedia110925/MainActivity.java
@@ -0,0 +1,101 @@
+package android.cauam.etimcauamarizatividademedia110925;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+     private EditText nota1, nota2, nota3, nota4, numeroFaltas;
+             private Button btnCalcuar;
+             private TextView resultado;
+             private String nome;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        initComponents();
+
+        btnCalcuar.setOnClickListener(view->{
+            validaCampos();
+            calculaMedia();
+            resultado.setText("Voce clicou no botao calcular!");
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void calculaMedia() {
+        double n1 = Double.parseDouble(nota1.getText().toString());
+        double n2 = Double.parseDouble(nota2.getText().toString());
+        double n3 = Double.parseDouble(nota3.getText().toString());
+        double n4 = Double.parseDouble(nota4.getText().toString());
+
+        double media = (n1 + n2 + n3 + n4) / 4;
+
+
+        if(media >7){
+            if(numeroFaltas < 20){
+                resultado.setTextColor(Color.parseColor("#438845"));
+                resultado.setText("Aluno aprovado com media: " + media);
+            }else{
+                resultado.setTextColor(Color.parseColor("#F44336"));
+                resultado.setText("Excesso de faltas: " + numeroFaltas);
+            }
+        }
+        else{
+            resultado.setTextColor(Color.parseColor("#F44336"));
+            resultado.setText("Aluno retido com media: " + media);
+        }
+    }
+
+    private void validaCampos() {
+        if(TextUtils.isEmpty(nota1.getText())){
+            nota1.setError("Este campo nao pode estar vazio");
+
+        }
+        else if(TextUtils.isEmpty(nota2.getText())){
+            nota1.setError("Este campo nao pode estar vazio");
+
+        }
+        else if(TextUtils.isEmpty(nota3.getText())){
+            nota1.setError("Este campo nao pode estar vazio");
+
+        }
+        else if(TextUtils.isEmpty(nota4.getText())){
+            nota1.setError("Este campo nao pode estar vazio");
+
+        }
+    }
+
+    private void initComponents() {
+        nota1 = findViewById(R.id.nota1);
+        nota2 = findViewById(R.id.nota2);
+        nota3 = findViewById(R.id.nota3);
+        nota4 = findViewById(R.id.nota4);
+
+        numeroFaltas = findViewById(R.id.numeroFalta);
+
+        btnCalcuar = findViewById(R.id.btnCalcular);
+
+        resultado = findViewById(R.id.txt_resultado);
+    }
+}
